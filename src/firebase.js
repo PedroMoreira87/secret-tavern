62b398e5c3e8adcae9eeb3ecbc7d3d4b20d459d4
pedro.moreira@aoatechpartners.com.br <pedrohmoreira87@gmail.com>
2021-11-02 18:17:50 -0300

    Rebase main

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import {initializeApp} from "firebase/app";
 import {getFirestore} from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,7 +17,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp)
+const storage = getStorage(firebaseApp);
 
-export {db};
+
+export {db, storage};
