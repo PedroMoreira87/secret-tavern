7fb43a9feceee775d42304c7924ad5ac8fa6f17c
david-sds <dave.sathler@gmail.com>
2021-11-07 00:08:33 -0300

    post-header default image

--- src/components/post/post-header/index.js
+++ src/components/post/post-header/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import './index.css'
+import { storage } from "../../../firebase"
+import { getStorage, ref, uploadBytes } from "firebase/storage"
+
+const getUserPhoto = (id) => {
+    
+}
 
 export default function PostHeader(props) {
     return (
         <div class="post-header">
             <div class="post-header-user">
-                <div class="post-header-user-photo">PIC</div>
+                <div class="post-header-user-photo">
+                    
+                    
+                </div>
                 <span>{props.first_name} {props.last_name}</span>
             </div>
         </div>
